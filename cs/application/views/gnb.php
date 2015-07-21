e7d9c4fda4817a0391c99b7e4d9fc631ddf0dd51
abhinav <abhinavsharma2308@gmail.com>
2015-07-21 20:44:37 +0530

    adding some missing files,.............

---
+++ cs/application/views/gnb.php
@@ -0,0 +1,35 @@
+<div class="gnb">
+<div class="logoWrap fl" href="javascript:void(0)"><img class="logo" src="<?php echo base_url();?>image/logo.jpg"/></div>
+<ul class="gnbMenu">
+<li class="singleMenu active"><a href="javascript:void(0)">Home</a></li>
+<li class="singleMenu"><a href="http://localhost/careerstep/html/events.html">Events</a></li>
+<li class="singleMenu">
+  <a href="javascript:void(0)">Jobs</a>
+  <div>
+  <ul>
+    <li><a href="index.html">Books</a></li>
+    <li><a href="index.html">eBooks</a></li>
+    <li><a href="index.html">Books For Study</a></li>
+    <li><a href="index.html">Audiobooks</a></li>
+  </ul>
+</div>
+</li>
+<li class="singleMenu"><a href="javascript:void(0)">About Us</a></li>
+<li class="singleMenu"><a href="javascript:void(0)">Email Us</a></li>
+<li class="singleMenu">
+  <a href="javascript:void()">Login</a>
+  <div>
+    <form name="login" action="#" method="POST">
+      <table class="login">
+        <tr><td>Email-Id :</td> <td><input type="text"></td></tr>
+        <tr><td>Password : </td> <td><input type="password"></td></tr>
+        <tr><td><button type="submit">Login</button></td> <td>
+        <a href="javascript:void(0)">Forgot Password</a></td></tr>
+      </table>
+    </form>
+  </div>
+</li>
+</ul>
+<marquee>GET THE BEST FROM THE BEST</marquee>
+
+    </div>
---
+++ cs/application/views/sticky.php
@@ -0,0 +1,18 @@
+    <div class="sticky">
+    <?php $this->load->view('gnb.php')?>
+    <div class="searchBar">
+      <div class="dispIB"><span>Search Jobs</span></div><br>
+      <div class="dispN">
+        <form>
+          <span>Search for</span>
+          <input type="text" name="kwd" placeholder="Keywords" />
+          <span>in</span>
+          <input type="text" name="loc" placeholder="Locations" />
+          <input type="submit" value="Search" />
+        </form>
+
+
+      </div>
+    </div>
+  </div>
+
